d58d8581dca8277440f39eeab5a6cca4b917888f
AmenanM <74093136+AmenanM@users.noreply.github.com>
2022-11-02 09:36:36 +0100

    Update run.py

--- run.py
+++ run.py
@@ -9,6 +9,8 @@ PAGES = {
     "fig":fig
 }
 st.sidebar.title('Navigation')
+st.set_page_config(page_icon="./Q quant logo.png", page_title="Quant AI Lab", layout="wide",
+                   initial_sidebar_state="expanded")
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 page = PAGES[selection]
 page.app()
